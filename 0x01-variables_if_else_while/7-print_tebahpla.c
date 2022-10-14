2537170ff75324fcfa2bf74bcb87fba67c031496
dacodingsalaf <dacodingsalafi@gmail.com>
2022-10-14 07:54:34 -0700

    sdsd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *     main - main block
+ *     Description: Prorder in lowercase.
+ *     Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
