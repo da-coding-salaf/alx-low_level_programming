d6afc4f71b1b48760bbf91dcc6ca09b48b4e4b17
dacodingsalaf <dacodingsalafi@gmail.com>
2022-10-14 04:36:42 -0700

    sdsd

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
 
 /**
- *
  *  * main - main block
- *
- *   * Description: Use `putchar` function to print the alphabet in lowercase.
- *
- *    * Return: 0
- *
- *     */
-
+ *   * Description: Use `putchar` function
+ *   * Return: 0
+ */
 int main(void)
-
 {
-
-		char c = 'a';
-
-			while (c <= 'z')
-
-					{
-
-								putchar(c);
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
 c++;
-	}
+}
 putchar('\n');
 return (0);
 }
