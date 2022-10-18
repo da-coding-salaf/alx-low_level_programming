36b5328a3ba1309a21281808c957b1015bad0a7d
dacodingsalaf <dacodingsalafi@gmail.com>
2022-10-18 13:06:13 -0700

    sdsd

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,12 @@
 /**
  *	 _isalpha - Check if character is an alphabetic character.
  *	@c: type int character
- *     Return: 1 if letter, lowercase or uppercase, and 0 otherwise
+ *      Return: 1 if letter, lowercase or uppercase, and 0 otherwise
  */
 int _isalpha(int c)
 {
 if ((c > 64 && c < 91) || (c > 96 && c < 123))
-		return (1);
+	return (1);
 else
-		return (0);
+	return (0);
 }
