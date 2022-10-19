cbb4079108c7035514493b0d6eaf82ca0a6ae10b
dacodingsalaf <dacodingsalafi@gmail.com>
2022-10-19 04:58:37 -0700

    sdsd

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <time.h>
+#include <stdlib.h>
 /**
  *	main - tests function that prints if integer is positive or negative
  *	Return: 0
@@ -6,7 +8,7 @@
 int main(void)
 {
 int i;
-i = 0;
+i = rand() - RAND_MAX / 2;
 positive_or_negative(i);
 return (0);
 }
