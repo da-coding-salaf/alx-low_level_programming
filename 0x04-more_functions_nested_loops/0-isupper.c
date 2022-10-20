da3916774a170b4590bcbbf5d1c6c7f53ca3c374
dacodingsalaf <dacodingsalafi@gmail.com>
2022-10-20 06:48:03 -0700

    sdsd

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <ctype.h>
+/**
+ *	_isupper - returns the largest of 3 numbers
+ *	@c: first integer
+ *	Return: 1
+ */
 int _isupper(int c)
 {
-return isupper(c);
-}	
+return (isupper(c));
+}
