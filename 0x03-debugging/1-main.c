536908c9d37268022c67d0ac6f9e2915967267ad
dacodingsalaf <dacodingsalafi@gmail.com>
2022-10-20 03:26:27 -0700

    sdsd

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,17 +8,11 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-/**
- *  main - causes an infinite loop
- *  while (i < 10)
-*{
-*	putchar(i);
-*}
-*	printf: asdf
-*/
+/*  while (i < 10)*/
+/*{*/
+/*	putchar(i);*/
+/*}*/
 
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
-
 }
