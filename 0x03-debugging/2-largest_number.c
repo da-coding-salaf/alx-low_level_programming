69f7f393b4f0e84174d58b71eae02f911da9f928
dacodingsalaf <dacodingsalafi@gmail.com>
2022-10-20 05:08:53 -0700

    sdsd

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *  largest_number - returns the largest of 3 numbers
  *  @a: first integer
@@ -8,6 +9,7 @@
  */
 int largest_number(int a, int b, int c)
 {
+
 int largest;
 if (a > b && a > c)
 {
@@ -21,5 +23,7 @@ else
 {
 	largest = c;
 }
+
 return (largest);
+
 }
